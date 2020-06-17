b9c203c394f1a4df7a8f2eb3a1c824cbb346418b
Brian Faust <hello@basecode.sh>
2020-06-17 15:35:36 +0300

    wip

--- src/app/services/HttpClient.ts
+++ src/app/services/HttpClient.ts
@@ -1,22 +1,34 @@
 import { Contracts } from "@arkecosystem/platform-sdk";
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { SocksProxyAgent } from "socks-proxy-agent";
 
 export class HttpClient implements Contracts.HttpClient {
-	readonly client: AxiosInstance;
+	#client: AxiosInstance;
+	readonly #socksHost: string = "socks5h://127.0.0.1:9050";
+
+	readonly #defaultConfig: AxiosRequestConfig = {
+		timeout: 2000,
+	};
 
 	public constructor() {
-		const config: AxiosRequestConfig = {
-			timeout: 2000,
-		};
+		this.#client = axios.create(this.#defaultConfig);
+	}
 
-		this.client = axios.create(config);
+	public withSocks(): HttpClient {
+		this.#client = axios.create({
+			...this.#defaultConfig,
+			httpAgent: new SocksProxyAgent(this.#socksHost),
+			httpsAgent: new SocksProxyAgent(this.#socksHost),
+		});
+
+		return this;
 	}
 
 	public async get(path: string, query: object = {}): Promise<Record<string, any>> {
-		return await this.client.get(path, { params: { ...query } });
+		return await this.#client.get(path, { params: { ...query } });
 	}
 
 	public async post(path: string, body: object, headers: object = {}) {
-		return await this.client.post(path, { body, headers });
+		return await this.#client.post(path, { body, headers });
 	}
 }

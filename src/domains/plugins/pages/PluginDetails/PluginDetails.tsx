00eb3302123bd4bdedc82be680bfe796e136d6b0
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-22 22:04:06 -0300

    feat(plugin-details): wip add reviews section

--- src/domains/plugins/pages/PluginDetails/PluginDetails.tsx
+++ src/domains/plugins/pages/PluginDetails/PluginDetails.tsx
@@ -1,6 +1,30 @@
 import { PluginHeader } from "domains/plugins/components/PluginHeader";
 import { PluginInfo } from "domains/plugins/components/PluginInfo";
+import { ReviewBox } from "domains/plugins/components/ReviewBox";
 import React from "react";
+
+const ratings = [
+	{
+		rating: 5,
+		votes: 156,
+	},
+	{
+		rating: 4,
+		votes: 194,
+	},
+	{
+		rating: 3,
+		votes: 25,
+	},
+	{
+		rating: 2,
+		votes: 42,
+	},
+	{
+		rating: 1,
+		votes: 7,
+	},
+];
 
 export const PluginDetails = () => (
 	<section className="h-full bg-theme-neutral-100">
@@ -17,6 +41,13 @@ export const PluginDetails = () => (
 			permissions={["Embedded Webpages", "API Requests", "Access to Profiles"]}
 			screenshots={[1, 2, 3]}
 		/>
-		<span>Reviews</span>
+		<div className="mt-5 bg-theme-background">
+			<div className="mx-10 py-24 grid grid-cols-2 grid-flow-col divide-x divide-theme-neutral-300">
+				<span className="col-span-2">Reviews</span>
+				<div className="pl-10">
+					<ReviewBox averageScore="4.3" ratings={ratings} totalAvaliations={347} />
+				</div>
+			</div>
+		</div>
 	</section>
 );

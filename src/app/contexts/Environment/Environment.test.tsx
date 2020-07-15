9833fb53b4a5e834d74d3d3f94bc190079a6d577
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-07-15 06:52:36 -0300

    test: add spec to check state

--- src/app/contexts/Environment/Environment.test.tsx
+++ src/app/contexts/Environment/Environment.test.tsx
@@ -2,26 +2,64 @@ import { ARK } from "@arkecosystem/platform-sdk-ark";
 import { Environment } from "@arkecosystem/platform-sdk-profiles";
 import { httpClient } from "app/services";
 import React from "react";
-import { render, screen, waitFor } from "testing-library";
 import { StubStorage } from "tests/mocks";
+import { act, fireEvent, render, waitFor } from "utils/testing-library";
 
-import { EnvironmentProvider } from "./Environment";
+import { EnvironmentProvider, useEnvironment } from "./Environment";
 
 describe("Environment Context", () => {
-	it("should render the wrapper properly", async () => {
+	it("should render the wrapper properly", () => {
 		const env = new Environment({ coins: { ARK }, httpClient, storage: new StubStorage() });
 
-		const { container, asFragment } = render(
+		const { container, asFragment, getByText } = render(
 			<EnvironmentProvider env={env}>
 				<span>Provider testing</span>
 			</EnvironmentProvider>,
+			{},
 		);
 
-		await waitFor(async () => {
-			await expect(screen.findByText("Provider testing")).resolves.toBeInTheDocument();
-		});
+		expect(getByText("Provider testing")).toBeInTheDocument();
 
 		expect(container).toBeTruthy();
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should rerender components when env updates", async () => {
+		const env = new Environment({ coins: {}, httpClient, storage: new StubStorage() });
+
+		const Details = () => {
+			const env = useEnvironment();
+			const count = env?.profiles().all().length;
+			return <h1>Counter {count}</h1>;
+		};
+
+		const Create = () => {
+			const env = useEnvironment();
+
+			const handleClick = () => {
+				env?.profiles().create("Test");
+				env?.persist();
+			};
+
+			return <button onClick={handleClick}>Create</button>;
+		};
+
+		const App = () => {
+			return (
+				<EnvironmentProvider env={env}>
+					<Details />
+					<Create />
+				</EnvironmentProvider>
+			);
+		};
+
+		const { getByRole } = render(<App />, {});
+
+		act(() => {
+			fireEvent.click(getByRole("button"));
+		});
+
+		expect(env.profiles().all()).toHaveLength(1);
+		await waitFor(() => expect(getByRole("heading")).toHaveTextContent("Counter 1"));
 	});
 });

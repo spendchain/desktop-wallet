474ddb797ae636ef7c329bc585ed225141cd31cd
kote-k <kotekumsishvili@gmail.com>
2020-12-02 01:54:09 -0700

    authkey location

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/AuthKey_C3HY5T92K2.p8 AuthKey_C3HY5T92K2.p8.gpg
+--output $HOME/secrets/AuthKey_C3HY5T92K2.p8 ./.github/scripts/AuthKey_C3HY5T92K2.p8.gpg

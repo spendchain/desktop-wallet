7c16e9fedf56b060007380b36f53709926585d73
kote-k <kotekumsishvili@gmail.com>
2020-12-02 01:40:08 -0700

    authkey workflow

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/AuthKey_C3HY5T92K2.p8 AuthKey_C3HY5T92K2.p8.gpg

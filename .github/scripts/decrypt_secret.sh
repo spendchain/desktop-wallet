78cfd61e57592b0a53dd435b624e98f66adad26a
kote-k <kotekumsishvili@gmail.com>
2020-12-02 01:58:34 -0700

    my_secret.json

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/AuthKey_C3HY5T92K2.p8 ./.github/scripts/AuthKey_C3HY5T92K2.p8.gpg
+--output $HOME/secrets/my_secret.json ./.github/scripts/my_secret.json.gpg

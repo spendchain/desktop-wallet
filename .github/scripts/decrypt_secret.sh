0e8b9e7ca076942d9566dec04769244cc1f5ad4f
kote-k <kotekumsishvili@gmail.com>
2020-12-02 02:16:58 -0700

    prepare draft

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# Decrypt the file
 mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
+
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $HOME/secrets/my_secret.json ./.github/scripts/my_secret.json.gpg
+
+mkdir -p ~/private_keys
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ~/private_keys/AuthKey.p8 ./.github/scripts/AuthKey.p8.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ~/private_keys/CSCKey.p12 ./.github/scripts/CSCKey.p12.gpg
